eadb8859fa21c162ff31c09d6a81c9bdd0c4c24e
Ali Ghaffari <aligh71@yahoo.com>
2024-05-06 12:53:04 +0330

    development of login api and creation of login form

---
+++ components/template/SigninPage.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
+function SigninPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const loginHandler = async () => {
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (!res.error) router.push("/");
+  };
+
+  return (
+    <div className="signin-form">
+      <h3>Login Form</h3>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={loginHandler}>Login</button>
+      <div>
+        <p>Create an account?</p>
+        <Link href="/signun">Sign un</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SigninPage;
---
+++ pages/signin.js
@@ -0,0 +1,8 @@
+import SigninPage from "@/components/template/SigninPage";
+import React from "react";
+
+function Signin() {
+  return <SigninPage />;
+}
+
+export default Signin;
