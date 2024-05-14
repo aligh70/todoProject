d95bb36187af6fcbdd23117c6ebbc5f61bde24e9
Ali Ghaffari <aligh71@yahoo.com>
2024-05-14 19:05:22 +0330

    create api to update the status of todos and add status change buttons

--- components/module/Tasks.js
+++ components/module/Tasks.js
@@ -1,6 +1,16 @@
 import { RiMastodonLine } from "react-icons/ri";
+import { BiRightArrow, BiLeftArrow } from "react-icons/bi";
 
-function Tasks({ data }) {
+function Tasks({ data, next, back, fetchTodos }) {
+  const changeStatus = async (id, status) => {
+    const res = await fetch("/api/todos", {
+      method: "PATCH",
+      body: JSON.stringify({ id, status }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    if (data.status === "success") fetchTodos();
+  };
   return (
     <div className="tasks">
       {data?.map((item) => (
@@ -8,6 +18,24 @@ function Tasks({ data }) {
           <span className={item.status}></span>
           <RiMastodonLine />
           <h4>{item.title}</h4>
+          <div>
+            {back ? (
+              <button
+                className="button-back"
+                onClick={() => changeStatus(item._id, back)}>
+                <BiLeftArrow />
+                Back
+              </button>
+            ) : null}
+            {next ? (
+              <button
+                className="button-next"
+                onClick={() => changeStatus(item._id, next)}>
+                Next
+                <BiRightArrow />
+              </button>
+            ) : null}
+          </div>
         </div>
       ))}
     </div>
